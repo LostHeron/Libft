27301a0722c3701f243ca564b8ff6c69cef04cd4
Jules Weber <jweber@student.42Lyon.fr>
2024-11-20 17:32:25 +0100

    [ADD] ft_calloc_start

---
+++ ft_calloc.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jweber <jweber@student.42Lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/20 14:57:13 by jweber            #+#    #+#             */
+/*   Updated: 2024/11/20 14:57:20 by jweber           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*res;
+	size_t	n3;
+
+	n3 = nmemb * size;
+	if (n3 < nmemb || n3 < size)
+		return (NULL);
+	res = malloc(nmemb * size);
+	if (res == NULL)
+		return (NULL);
+}

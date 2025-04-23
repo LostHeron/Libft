4a7bb6f1967831e7880ba49489176e17f8281c0a
Jules Weber <jweber@student.42Lyon.fr>
2025-04-23 15:41:36 +0200

    [MOD] modified ft_power to make it works on double (cannot perform power on decimal exponent yet)

--- src/math/ft_power.c
+++ src/math/ft_power.c
@@ -6,14 +6,31 @@
 /*   By: jweber <jweber@student.42Lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 09:26:31 by jweber            #+#    #+#             */
-/*   Updated: 2025/02/14 09:28:07 by jweber           ###   ########.fr       */
+/*   Updated: 2025/04/23 15:34:45 by jweber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_power(int nb, int power)
+#include <stdlib.h>
+
+int	ft_power_int(int nb, int power)
 {
 	int	i;
 	int	res;
+
+	res = 1;
+	i = 0;
+	while (i < power)
+	{
+		res = res * nb;
+		i++;
+	}
+	return (res);
+}
+
+double	ft_power(double nb, size_t power)
+{
+	size_t	i;
+	double	res;
 
 	res = 1;
 	i = 0;

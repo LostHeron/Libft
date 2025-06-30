d5ed588d0123104801f18ef59dcd4db3c7a56672
Jules Weber <jweber@student.42Lyon.fr>
2025-06-30 15:08:32 +0200

    [ADD] allowing ft_strjoin function to take a NULL pointer

--- src/string/ft_strjoin.c
+++ src/string/ft_strjoin.c
@@ -13,7 +13,73 @@
 #include "ft_string.h"
 #include "ft_standard.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+static char	*case_none_null(char const *s1, char const *s2);
+static char	*case_first_null(char const *s2);
+static char	*case_second_null(char const *s1);
+
+char	*ft_strjoin_free_first(char const *s1, char const *s2)
+{
+	char	*res;
+
+	if (s1 == NULL && s2 == NULL)
+	{
+		res = ft_malloc(1 * sizeof(char));
+		return (res);
+	}
+	else if (s1 == NULL)
+	{
+		return (case_first_null(s2));
+	}
+	else if (s2 == NULL)
+	{
+		return (case_second_null(s1));
+	}
+	else
+	{
+		return (case_none_null(s1, s2));
+	}
+}
+
+static char	*case_first_null(char const *s2)
+{
+	size_t	s2_len;
+	size_t	i;
+	char	*res;
+
+	s2_len = ft_strlen(s2);
+	res = ft_malloc((s2_len + 1) * sizeof(char));
+	if (res == NULL)
+		return (NULL);
+	i = 0;
+	while (i < s2_len)
+	{
+		res[i] = s2[i];
+		i++;
+	}
+	return (res);
+}
+
+static char	*case_second_null(char const *s1)
+{
+	size_t	s1_len;
+	size_t	i;
+	char	*res;
+
+	s1_len = ft_strlen(s1);
+	res = ft_malloc((s1_len + 1) * sizeof(char));
+	if (res == NULL)
+		return (NULL);
+	i = 0;
+	while (i < s1_len)
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
+
+static char	*case_none_null(char const *s1, char const *s2)
 {
 	size_t	s1_len;
 	size_t	s2_len;
@@ -25,40 +91,17 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	res = ft_malloc((s1_len + s2_len + 1) * sizeof(char));
 	if (res == NULL)
 		return (NULL);
-	i = -1;
-	while (++i < s1_len)
+	i = 0;
+	while (i < s1_len)
+	{
 		res[i] = s1[i];
-	i--;
-	while (++i < s1_len + s2_len)
+		i++;
+	}
+	while (i < s1_len + s2_len)
+	{
 		res[i] = s2[i - s1_len];
+		i++;
+	}
 	res[i] = '\0';
 	return (res);
 }
-
-/*
-#include <stdio.h>
-
-static void	check(char const *s1, char const *s2)
-{
-	char	*res;
-
-	res = ft_strjoin(s1, s2);
-	printf("s1 = %s\n", s1);
-	printf("s2 = %s\n", s2);
-	printf("res = %s\n", res);
-	printf("\n");
-	free(res);
-}
-
-int	main(void)
-{
-	check("Yo", "LaTeam");
-	check("", "");
-	check("qspifjqsdf", "");
-	check("qspifjqsdf", "ehehmongars");
-
-	void	*a;
-	a = NULL;
-	check(a + 1, "yo");
-}
-*/
--- src/string/ft_strjoin_free_first.c
+++ src/string/ft_strjoin_free_first.c
@@ -13,7 +13,75 @@
 #include "ft_string.h"
 #include "ft_standard.h"
 
+static char	*case_none_null(char const *s1, char const *s2);
+static char	*case_first_null(char const *s2);
+static char	*case_second_null(char const *s1);
+
 char	*ft_strjoin_free_first(char const *s1, char const *s2)
+{
+	char	*res;
+
+	if (s1 == NULL && s2 == NULL)
+	{
+		res = ft_malloc(1 * sizeof(char));
+		return (res);
+	}
+	else if (s1 == NULL)
+	{
+		return (case_first_null(s2));
+	}
+	else if (s2 == NULL)
+	{
+		return (case_second_null(s1));
+	}
+	else
+	{
+		return (case_none_null(s1, s2));
+	}
+}
+
+static char	*case_first_null(char const *s2)
+{
+	size_t	s2_len;
+	size_t	i;
+	char	*res;
+
+	s2_len = ft_strlen(s2);
+	res = ft_malloc((s2_len + 1) * sizeof(char));
+	if (res == NULL)
+		return (NULL);
+	i = 0;
+	while (i < s2_len)
+	{
+		res[i] = s2[i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
+
+static char	*case_second_null(char const *s1)
+{
+	size_t	s1_len;
+	size_t	i;
+	char	*res;
+
+	s1_len = ft_strlen(s1);
+	res = ft_malloc((s1_len + 1) * sizeof(char));
+	if (res == NULL)
+		return (NULL);
+	i = 0;
+	while (i < s1_len)
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	res[i] = '\0';
+	free((void *)s1);
+	return (res);
+}
+
+static char	*case_none_null(char const *s1, char const *s2)
 {
 	size_t	s1_len;
 	size_t	s2_len;
